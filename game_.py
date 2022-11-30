3fbed4fed01fdfcb3ce26113e0c6713cf5609282
KJS0 <112676891+KJS0@users.noreply.github.com>
2022-11-30 14:42:05 +0900

    20221130 1441

--- game_.py
+++ game_.py
@@ -5,6 +5,7 @@ import math
 import random
 
 # gTTS
+import speech_recognition as sr
 from glob import glob
 from io import BytesIO
 from gtts import gTTS
@@ -14,6 +15,7 @@ from pydub.playback import play
 flag1 = []
 flag2 = []
 tmp_tts = []
+voice = ''
 
 """
 메인 화면
@@ -44,6 +46,7 @@ def gameStart():
     
     level = 1 # 초기 레벨
     life = 3 # 초기 생명 수
+    chance = 3 # 기회
     ran1 = random.randrange(0, 2) # 초기 깃발 색상
     ran2 = 0 # 질문 ex) 올려, 내려, 내리지 말고 ...
     tmp_tts = ''
@@ -51,7 +54,26 @@ def gameStart():
     FLAGS_TXT = [ "청기", "백기" ]
     QUESTION_TXT = [ "올려", "올리고", "올리지 말고", "내려", "내리고", "내리지 말고" ]
     
+    print("청기백기 게임에 참여하는 여러분! 환영합니다")
+    print("규칙은 다음과 같습니다.")
+    #tts("청기백기 게임에 참여하는 여러분! 환영합니다")
+    #tts("규칙은 다음과 같습니다.")
+    
+    os.system("cls")
+    print("1. 지시된 명령을 듣고, 최종적으로 올리거나 내려야 할 깃발을 음성으로 말해야합니다.")
+    print("2. 음성은 ""백기 올려""와 같이 두 단어로만 대답해야합니다.")
+    print("3. 만약 음성을 인식하지 못하는 상황을 대비해, 세번까지 기회가 주어집니다.")
+    #tts("1. 지시된 명령을 듣고, 최종적으로 올리거나 내려야 할 깃발을 음성으로 말해야합니다.")
+    #tts("2. 음성은 ""백기 올려""와 같이 두 단어로만 대답해야합니다.")
+    #tts("3. 만약 음성을 인식하지 못하는 상황을 대비해, 세번까지 기회가 주어집니다.")
+    
+    os.system("cls")
+    tts("지금부터 게임을 시작하겠습니다.")
+    print("지금부터 게임을 시작하겠습니다.")
+    
+    
     while True:
+        chance = 3
         os.system("cls")
         if life == 0:
             print("GAME OVER")
@@ -59,13 +81,15 @@ def gameStart():
             time.sleep(5)
             break
         
-        flag1 = False
+        flag1 = [ False, False ]
         flag2 = [ False, False ]
         
         level_num = math.floor(level ** 0.5) # 레벨 함수 수식 (수정 가능)
+        tts(f"{level} 단계 시작")
         print(f"{level} 레벨, {life}")
         
         print(FLAGS_TXT[ran1])
+        tmp_tts = tmp_tts + FLAGS_TXT[ran1]
         for i in range(1, level_num + 1):
             if level_num == 1 or i == level_num:
                 ran2 = random.choice([0, 3]) # 올려, 내려
@@ -73,22 +97,65 @@ def gameStart():
                 ran2 = random.choice([1, 2, 4, 5]) # 올리고, 올리지 말고, 내리고, 내리지 말고
             
             if 0 <= ran2 <= 1: # 올려, 올리고
-                flag1 = True
+                flag1[ran1 - 1] = True
             elif 3 <= ran2 <= 4: # 내려, 내리고
-                flag1 = False
+                flag1[ran1 - 1] = False
             tmp_tts = tmp_tts + (QUESTION_TXT[ran2] + ' ')
             print(QUESTION_TXT[ran2], end = ' ')
         
+        global voice
         tts(tmp_tts)
-        if flag1 == flag2[len(flag2) - 1]:
-            print(f"\n{level} 레벨 통과!")
+        
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source, timeout=5)
+                
+        voice = r.recognize_google(audio, language='ko-KR')
+        if voice[0] == "청기":
+            if voice[1] == "올려":
+                flag2[0] = True
+            elif voice[1] == "내려":
+                flag2[0] = False
+        elif voice[0] == "백기":
+            if voice[1] == "올려":
+                flag2[1] = True
+            elif voice[1] == "내려":
+                flag2[1] = False
+            else:
+                tts("뭐라고 했는지 이해가 안되요")
+                chance -= 1
+                
+            if chance != 0:
+                chance -= 1
+            elif chance == 0:
+                break
+            
+        print('\n' + voice)
+        
+        time.sleep(3)
+        
+        if flag1 == flag2:
+            os.system("cls")
+            print(f"\n{level} 단계 통과!")
+            tts(f"{level} 단계 통과!")
+            level += 1
             
             if level % 5 == 0: # x5, x0 레벨 통과 시 기회 1회 더 부여
                 print(f"LIFE +1")
+                tts(f"{level} 단계를 통과했으므로, 기회를 한번 더 드리겠습니다.")
                 life += 1
+        elif chance == 0 or flag1 != flag2:
+            os.system("cls")
+            life -= 1
+            
+            print(f"{level} 단계 통과 실패!")
+            print(f"앞으로 {life}번 남았습니다.")
+            
+            tts(f"{level} 단계 통과 실패!")
+            tts(f"앞으로 {life}번 남았습니다.")
         
         tmp_tts = ' '
-        level += 1
+        
         time.sleep(1.5)
         print("\n")
 
@@ -99,7 +166,7 @@ def gameExit():
     sys.exit(0)
 
 """
-TTS 구현 (현재 gTTS 작동 안함!)
+TTS 구현 (gTTS 2.3.0)
 """
 def tts(word, toSlow=False):
     tts = gTTS(text=word, lang="ko", slow=toSlow)
